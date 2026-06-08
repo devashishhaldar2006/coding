4fcb6a2da78921a51414d1a9313855cd99f26697
Devashish <workfordevashishhaldar@gmail.com>
2026-06-09 01:13:21 +0530

    add binary to decimal conversion function and implement even number bitwise ORs solution

--- c++/bitwise operators/tempCodeRunnerFile.cpp
+++ c++/bitwise operators/tempCodeRunnerFile.cpp
@@ -1,1 +1,10 @@
-((k<<1)-1)^temp;
+int binary_to_decimal(string &binary){
+//     int n=binary.size();
+//     int result=0;
+//     for(int i=n-1;i>=0;i--){
+//       char ch=binary[i];
+//       int num=ch-'0';
+//       result=result+num*(1<<(n-i-1));//n-index-1 //2 ki power me nikal rhe
+//     }
+//     return result;
+// }
